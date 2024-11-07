6a51175e7bfe7ec48afa7e299326dbfa2d015684
Nishant.Pande <Nishant.Pande@ESDS003568NP>
2024-11-07 13:29:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
 import Dashboard from './Components/Dashboard';
 import SignUp from './Components/SignUp';
 import AddProduct from './Components/AddProductForm';
+import 'process';
 
 function App() {
-  // Assuming you have a simple authentication state (can be managed via context or useState)
-  const isAuthenticated = false; // Replace this with actual authentication logic
-
   return (
     <div className="App">
       <Router>
         <Routes>
-          {/* Route for Login page */}
-          <Route path="/" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Login />} />
-          
-          {/* Route for SignUp page */}
+          <Route path="/" element={<Login />} />
           <Route path="/SignUp" element={<SignUp />} />
-          
-          {/* Route for Dashboard page */}
-          <Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Navigate to="/" />} />
-          
-          {/* Catch-all route (if user tries to access other paths) */}
-          <Route path="/*" element={<Navigate to="/" />} />
+          <Route path="/*" element={<Dashboard />} /> 
         </Routes>
       </Router>
     </div>
