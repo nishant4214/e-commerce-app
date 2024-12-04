768024133590492e53e508cbea35298f6c90605f
Nishant.Pande <Nishant.Pande@ESDS003568NP>
2024-12-04 17:31:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+// App.js
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login';
@@ -6,30 +8,27 @@ import CustomerDashboard from './Components/CustomerDashboard';
 import SignUp from './Components/SignUp';
 import ForgotPassword from './Components/ForgotPassword';
 import ResetPassword from './Components/ResetPassword';
+import 'process';
 import { AuthProvider } from './AuthContext';
-import { CartProvider } from './CartContext';
-
+import { CartProvider, useCart }  from './CartContext';
 function App() {
   return (
-    <CartProvider> {/* Wrap the entire app inside CartProvider */}
-      <div className="App">
-        <Router basename="/e-commerce-app">
-          <Routes>
-            {/* Non-authenticated routes */}
-            <Route path="/" element={<Login />} />
-            <Route path="/SignUp" element={<SignUp />} />
-            <Route path="/ForgotPassword" element={<ForgotPassword />} />
-            <Route path="/ResetPassword" element={<ResetPassword />} />
-
-            {/* Authenticated routes wrapped with AuthProvider */}
-            <AuthProvider>
+    <AuthProvider>
+      <CartProvider> {/* Wrap the entire app inside CartProvider */}
+        <div className="App">
+          <Router basename="/e-commerce-app">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/SignUp" element={<SignUp />} />
+              <Route path="/ForgotPassword" element={<ForgotPassword />} />
+              <Route path="/ResetPassword" element={<ResetPassword />} />
               <Route path="/Dashboard/*" element={<Dashboard />} />
               <Route path="/customer-dashboard/*" element={<CustomerDashboard />} />
-            </AuthProvider>
-          </Routes>
-        </Router>
-      </div>
-    </CartProvider>
+            </Routes>
+          </Router>
+        </div>
+      </CartProvider>
+    </AuthProvider>
   );
 }
 
